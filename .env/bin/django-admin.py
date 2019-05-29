8da9c92f8ee01b4f7cd01ce56be1137fb62e815a
John Oro <johnmichaeloro@gmail.com>
2019-05-29 11:18:06 -0600

    installed django and started project

---
+++ .env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/johnoro/sei/first_django_project/first_django_app/.env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
